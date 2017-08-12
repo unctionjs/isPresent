2a9e1270e221b449a58f8b3f327898ba82e6d054
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:11:18 -0700

    Woops, totally fucked up these paths

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-undefined, no-magic-numbers */
 import {test} from "tap"
 
-import isPopulated from "./source"
+import isPresent from "./source"
 
 test("array", ({equal, end}) => {
   equal(isPresent([]), true)
