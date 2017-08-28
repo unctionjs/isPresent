3544b97ffe933085b86ea49a820d59179660b03b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-27 18:51:01 -0700

    Going back to how other javascript packages do it, makes it eaiser on everyone :/

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-undefined, no-magic-numbers */
 import {test} from "tap"
 
-import isPresent from "./source"
+import isPresent from "./"
 
 test("array", ({equal, end}) => {
   equal(isPresent([]), true)
