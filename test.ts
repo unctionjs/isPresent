1220ba1c77fb5424484af17a5e26d4f46f84bd22
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:48 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-undefined, no-magic-numbers */
+import isPresent from "./index.ts";
+
+test("array", () => {
+  expect(isPresent([])).toBe(true);
+});
+
+test("object", () => {
+  expect(isPresent({})).toBe(true);
+});
+
+test("string", () => {
+  expect(isPresent("")).toBe(true);
+});
+
+test("zero", () => {
+  expect(isPresent(0)).toBe(true);
+});
+
+test("true", () => {
+  expect(isPresent(true)).toBe(true);
+});
+
+test("false", () => {
+  expect(isPresent(false)).toBe(true);
+});
+
+test("null", () => {
+  expect(isPresent(null)).toBe(false);
+});
+
+
+test("undefined", () => {
+  expect(isPresent(undefined)).toBe(false);
+});
